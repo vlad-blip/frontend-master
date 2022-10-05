f9a51a5787bb2a1eac7ce4465d55be3b54ef8541
Vladyslav Lazarenko <ezio2506@gmail.com>
2022-10-05 18:09:44 +0300

    everything is done except few small things

--- Sunnyside agency landing page. HTML CSS JS/sunnyside-agency-landing-page-main/burger.js
+++ Sunnyside agency landing page. HTML CSS JS/sunnyside-agency-landing-page-main/burger.js
@@ -2,9 +2,11 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-list');
 
 burger.addEventListener('click', () => {
-    if (nav.style.display === 'none'){
-        nav.style.display = 'flex';
+    if (nav.classList.contains('nav-list--openned')){
+        nav.classList.remove('nav-list--openned');
+        nav.classList.add('nav-list--closed');
     } else {
-        nav.style.display = 'none';
+        nav.classList.remove('nav-list--closed');
+        nav.classList.add('nav-list--openned');
     }
 });
